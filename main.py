4a730a98f94eb9bea618f2b9d54d8c863276af41
clemenskainz <clemens.kainz@gmail.com>
2020-07-19 15:05:42 +0200

    Added an Entry field, from where you can change the Volume without slider. Doesn't work yet.

--- main.py
+++ main.py
@@ -5,15 +5,31 @@ from tkinter import ttk
 import time
 
 slider = None
+volEntry = None
 
 def gui():
 
+    def setvolume():
+        global slider
+        global volEntry
+
+        volume = volEntry.get()
+        slider.set(volume)
+
     def audioSlider(tab1):
         global slider
+        global volEntry
 
-        slider = tk.Scale(tab1, from_=0, to=200, length=400, orient=tk.HORIZONTAL)
+        slider = tk.Scale(tab1, from_=0, to=100, length=400, orient=tk.HORIZONTAL)
         slider.set(50)
-        slider.pack()
+
+        volEntry = tk.Entry(tab1)
+
+        volButton = tk.Button(tab1, text="Set Volume", command=setvolume)
+
+        slider.place(x=90, y=0)
+        volEntry.place(x=92, y=60)
+        volButton.place(x=230, y=57)
 
 
     def tabSetup():
@@ -39,22 +55,33 @@ def gui():
     root = tk.Tk()
     tabSetup()
 
+    #frame = tk.Frame(root)
+    #frame.pack()
+
     root.title("Common Settings")
     root.geometry(f"{width}x{height}")
-    root.minsize(500, 300)
+    root.resizable(0, 0)
     root.iconbitmap(r"icon.ico")
 
-    
 
     root.mainloop()
 
 def audio():
+    global volEntry
     global slider
+    oldSlider =  None
 
     while True:
-        if slider != None:
-            print(slider.get())
-        time.sleep(0.05)
+        if slider != oldSlider:
+            sliderData = slider.get()
+            print(oldSlider)
+
+            volEntry.delete(0, tk.END)
+            volEntry.insert(0, str(sliderData))
+
+            oldSlider = sliderData
+
+        time.sleep(0.01)
     
 
 guiT = threading.Thread(target=gui)
